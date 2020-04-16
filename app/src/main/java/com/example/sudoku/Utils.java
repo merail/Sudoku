fa421b8ca9d0c9a77b286a11dafeec9b9f51c431
rail <37214897+merail@users.noreply.github.com>
2020-04-16 22:12:53 +0300

    add dbhelper class

--- app/src/main/java/com/example/sudoku/Utils.java
+++ app/src/main/java/com/example/sudoku/Utils.java
@@ -1,6 +1,7 @@
 package com.example.sudoku;
 
 import android.view.View;
+import com.example.sudoku.FeedReaderContract.FeedEntry;
 
 class Utils {
     static final int currentApiVersion = android.os.Build.VERSION.SDK_INT;
@@ -11,4 +12,13 @@ class Utils {
             | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
             | View.SYSTEM_UI_FLAG_FULLSCREEN
             | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+
+    static final String SQL_CREATE_ENTRIES =
+            "CREATE TABLE " + FeedEntry.TABLE_NAME + " (" +
+                    FeedEntry._ID + " INTEGER PRIMARY KEY," +
+                    FeedEntry.COLUMN_NAME_TITLE + " TEXT," +
+                    FeedEntry.COLUMN_NAME_SUBTITLE + " TEXT)";
+
+    static final String SQL_DELETE_ENTRIES =
+            "DROP TABLE IF EXISTS " + FeedEntry.TABLE_NAME;
 }
