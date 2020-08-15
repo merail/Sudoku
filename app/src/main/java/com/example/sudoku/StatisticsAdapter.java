84cd8910ebd2e180fe3758af1a1ba17dc4fa5fa1
rail <37214897+merail@users.noreply.github.com>
2020-08-15 13:49:25 +0300

    add statistics to statistics activity

--- app/src/main/java/com/example/sudoku/StatisticsAdapter.java
+++ app/src/main/java/com/example/sudoku/StatisticsAdapter.java
@@ -8,6 +8,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -31,18 +32,32 @@ public class StatisticsAdapter extends RecyclerView.Adapter<StatisticsAdapter.Vi
     @SuppressLint("ClickableViewAccessibility")
     @Override
     public void onBindViewHolder(@NonNull StatisticsAdapter.ViewHolder holder, final int position) {
-
+        switch(position)
+        {
+            case 0:
+                holder.statistics.setText("Started games");
+                break;
+            case 1:
+                holder.statistics.setText("Completed games");
+                break;
+            case 2:
+                holder.statistics.setText("Win rate");
+                break;
+        }
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return 3;
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder {
+        private TextView statistics;
 
         ViewHolder(View itemView) {
             super(itemView);
+
+            statistics = itemView.findViewById(R.id.statistics);
         }
     }
 }
